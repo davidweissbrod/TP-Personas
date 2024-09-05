c7c9a4345ca799fc4ec29055265e7c2599d223e8
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-09-05 12:01:38 -0300

    banca

--- tp-personas/src/app/page.js
+++ tp-personas/src/app/page.js
@@ -1,19 +1,57 @@
+"use client";
 import styles from "./styles/page.module.css";
 import personas from "./Personas";
-import Link from "next/link";
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Home() {
-console.log(personas)
+  const [show, setShow] = useState(false);
+  const [selectedPersona, setSelectedPersona] = useState(null);
+
+  const handleClose = () => {
+    setShow(false);
+    setSelectedPersona(null);
+  };
+
+  const handleShow = (persona) => {
+    setSelectedPersona(persona);
+    setShow(true);
+  };
+
   return (
     <div className={styles.lista}>
-        <h1>Listado de Personas</h1>
-        <ul>
-          {personas.map((persona, index) => (
-            <>
-              <li key={index}>{persona.nombre}, {persona.apellido} <Link href={`/persona/${persona.id}`} className={styles.link}>Mas info</Link></li> 
-            </> 
-          ))}
-        </ul>
+      <h1>Listado de Personas</h1>
+      <ul>
+        {personas.map((persona, index) => (
+          <li key={index}>
+            {persona.nombre}, {persona.apellido}
+            <Button variant="primary" onClick={() => handleShow(persona)}>
+              Más Info
+            </Button>
+          </li>
+        ))}
+      </ul>
+
+      {selectedPersona && (
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Información</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p><strong>Nombre:</strong> {selectedPersona.nombre}</p>
+            <p><strong>Apellido:</strong> {selectedPersona.apellido}</p>
+            <p><strong>Email:</strong> {selectedPersona.email}</p>
+            <p><strong>Edad:</strong> {selectedPersona.edad}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
   );
 }
+
