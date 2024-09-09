4c9e383286e9efdabbddf554f9bcd05361b38af8
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-09-09 08:20:59 -0300

    LISTOOOOOOO

--- tp-personas/src/app/page.js
+++ tp-personas/src/app/page.js
@@ -2,8 +2,10 @@
 import styles from "./styles/page.module.css";
 import personas from "./Personas";
 import { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   const [show, setShow] = useState(false);
@@ -22,16 +24,23 @@ export default function Home() {
   return (
     <div className={styles.lista}>
       <h1>Listado de Personas</h1>
-      <ul>
+      <div className={styles.cardContainer}>
         {personas.map((persona, index) => (
-          <li key={index}>
-            {persona.nombre}, {persona.apellido}
-            <Button variant="primary" onClick={() => handleShow(persona)}>
-              Más Info
-            </Button>
-          </li>
+          <Card key={index} className={styles.card}>
+            <Card.Body>
+              <Card.Title>{persona.nombre} {persona.apellido}</Card.Title>
+              <Card.Text>
+                <strong>Email:</strong> {persona.email}<br />
+                <strong>Edad:</strong> {persona.edad}
+              </Card.Text>
+              <Button variant="primary" onClick={() => handleShow(persona)}>
+                Más Info
+              </Button>
+            </Card.Body>
+          </Card>
         ))}
-      </ul>
+        
+      </div>
 
       {selectedPersona && (
         <Modal show={show} onHide={handleClose}>
@@ -45,7 +54,7 @@ export default function Home() {
             <p><strong>Edad:</strong> {selectedPersona.edad}</p>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant="danger" onClick={handleClose}>
               Cerrar
             </Button>
           </Modal.Footer>
